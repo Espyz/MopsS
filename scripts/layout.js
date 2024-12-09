1a2abaed9abbe091a6bb1ec269b3212783e368a3
Espyz <truharandrey1015@gmail.com>
2024-12-09 17:52:43 +0500

    Готовый адаптив header

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,7 +1,11 @@
 const input = document.getElementById('finder');
+const input2 = document.getElementById('finder2');
 
 input.addEventListener('click', newInput);
 input.addEventListener('blur', endInput);
+
+input2.addEventListener('click', newInput2);
+input2.addEventListener('blur', endInput2);
 
 function newInput() {
     if (input.value === 'Поиск по сайту...') {
@@ -12,6 +16,18 @@ function newInput() {
 function endInput() {
     if (input.value.trim() === '') {
         input.value = 'Поиск по сайту...';
+    }
+}
+
+function newInput2() {
+    if (input2.value === 'Поиск по сайту...') {
+        input2.value = '';
+    }
+}
+
+function endInput2() {
+    if (input2.value.trim() === '') {
+        input2.value = 'Поиск по сайту...';
     }
 }
 
@@ -27,4 +43,26 @@ function goToHome(event) {
     if (mainFile !== 'index.html') {
         location.replace('../index.html');
     }
+}
+
+const burger = document.getElementById('burger');
+const closeBurger = document.getElementById('close-burger');
+
+burger.addEventListener('click', openMenu);
+closeBurger.addEventListener('click', closeMenu);
+
+function openMenu() {
+    console.log('open menu');
+    document.getElementById('header-menu').classList.add('flex');
+    document.getElementById('header-default').classList.add('d-none');
+    document.getElementById('header-menu').classList.remove('d-none');
+    document.getElementById('header-default').classList.remove('flex');
+}
+
+function closeMenu() {
+    console.log('close menu');
+    document.getElementById('header-menu').classList.add('d-none');
+    document.getElementById('header-default').classList.add('flex');
+    document.getElementById('header-menu').classList.remove('flex');
+    document.getElementById('header-default').classList.remove('d-none');
 }
